98988fd6e8a8a9882eee5e4e1d26a027c8957431
xandersherry <github@xanderland.com>
2015-02-01 16:20:08 -0500

    Update assembly versions to 0.9.50201.1

--- CryptoCore/Properties/AssemblyInfo.cs
+++ CryptoCore/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CryptoCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A cryptographic library for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Windows User")]
+[assembly: AssemblyCompany("Xeres Security")]
 [assembly: AssemblyProduct("CryptoCore")]
-[assembly: AssemblyCopyright("Copyright © Windows User 2014")]
+[assembly: AssemblyCopyright("Copyright © Xander Sherry 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.50201.1")]
+[assembly: AssemblyFileVersion("0.9.50201.1")]
--- CryptoCoreTests/Properties/AssemblyInfo.cs
+++ CryptoCoreTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CryptoCoreTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CryptoCore Tests")]
+[assembly: AssemblyDescription("Tests for the CryptoCore library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Windows User")]
-[assembly: AssemblyProduct("CryptoCoreTests")]
-[assembly: AssemblyCopyright("Copyright © Windows User 2014")]
+[assembly: AssemblyCompany("Xeres Security")]
+[assembly: AssemblyProduct("CryptoCore")]
+[assembly: AssemblyCopyright("Copyright © Xander Sherry 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.50201.1")]
+[assembly: AssemblyFileVersion("0.9.50201.1")]
